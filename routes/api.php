af6b3da95d5e3c1d21c73c278a72e265fef2ddc8
Olalekan Agbaje <olalekanagbaje@gmail.com>
2021-09-28 17:46:40 +0100

    updated api routes for create/updatea/delete

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,15 @@ use App\Http\Controllers\OfficeController;
 |
 */
 
+// Tags
 Route::get('/tags', TagController::class);
+
+// Offices
 Route::get('/offices', [OfficeController::class, 'index'])->name('offices.index');
 Route::get('/offices/{office}', [OfficeController::class, 'show'])->name('office.show');
+Route::post('/offices', [OfficeController::class, 'create'])
+    ->middleware(['auth:sanctum', 'verified'])->name('offices.create');
+Route::put('/offices/{office}', [OfficeController::class, 'update'])
+    ->middleware(['auth:sanctum', 'verified'])->name('offices.update');
+Route::delete('/offices/{office}', [OfficeController::class, 'destroy'])
+    ->middleware(['auth:sanctum', 'verified'])->name('offices.delete');
