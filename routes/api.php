3c9e5b5403c4d9719723ae886d5192c3ea8b84ee
Olalekan Agbaje <olalekanagbaje@gmail.com>
2021-10-01 10:23:03 +0100

    add office images creation route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TagController;
 use App\Http\Controllers\OfficeController;
+use App\Http\Controllers\OfficeImageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,3 +29,6 @@ Route::put('/offices/{office}', [OfficeController::class, 'update'])
     ->middleware(['auth:sanctum', 'verified'])->name('offices.update');
 Route::delete('/offices/{office}', [OfficeController::class, 'destroy'])
     ->middleware(['auth:sanctum', 'verified'])->name('offices.delete');
+
+Route::post('/offices/{office}/images', [OfficeImageController::class, 'store'])
+    ->middleware(['auth:sanctum', 'verified'])->name('offices.images.store');
